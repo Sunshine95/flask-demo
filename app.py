ca75cb06bdc4922393b82d9d0a1fd7d98cd7593c
Dushyant Goel <dushyant95goel@gmail.com>
2021-05-03 15:04:33 +0530

    added minimal database

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+db = SQLAlchemy(app)
+
+class Tabl(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    status = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return '<Item %r>' % self.id
 
 @app.route('/')
 def index():
