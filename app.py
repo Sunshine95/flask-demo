fc778c9637603ee95e3622558e38c13b1bf2ee8b
Dushyant Goel <dushyant95goel@gmail.com>
2021-05-03 17:40:07 +0530

    application complete

--- app.py
+++ app.py
@@ -29,6 +29,31 @@ def index():
         items = Item.query.order_by(Item.status).all()
         return render_template('index.html', items=items)
 
+@app.route('/delete/<int:id>')
+def delete(id):
+    item_to_delete = Item.query.get_or_404(id)
+
+    try:
+        db.session.delete(item_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'Error: Failed to delete Item'
+
+@app.route('/update/<int:id>')
+def update(id):
+    item_to_update = Item.query.get_or_404(id)
+    new_status = (item_to_update.status + 1) % 3
+    item_to_update.status = new_status
+
+    try:
+        db.session.commit()
+        return redirect('/')
+    except Exception as inst:
+        print(inst.args)
+        print(inst)
+        return 'Error: Failed to update Item status'
+
 if __name__ == "__main__":
     app.run(debug=True)
 
